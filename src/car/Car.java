c84fcbce7b6fa2c26c165af4fb8a251f1bbbd623
Abdelrhmann-Tarek <abdelrahmantarek696@gmail.com>
2026-04-24 17:48:14 +0300

    feat: add engine and speed attributes to Car class

--- src/car/Car.java
+++ src/car/Car.java
@@ -1,5 +1,20 @@
 package car;
 
+import engines.Engine;
+
 public class Car {
+    private Engine engine;
+    private int speed = 0;
+
+    public Car(Engine engine) {
+        this.engine = engine;
+    }
+
+    public void start() {
+        speed = 0;
+        engine.setSpeed(speed);
+        System.out.println("Car started at speed : " + speed);
+
+    }
 
 }
