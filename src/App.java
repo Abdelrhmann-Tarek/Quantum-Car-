a2a85679f935f919ff3118eae35bb9cd9158ee12
Abdelrhmann-Tarek <abdelrahmantarek696@gmail.com>
2026-04-24 18:09:44 +0300

    feat: enhance App class with comprehensive car testing and engine replacement functionality

--- src/App.java
+++ src/App.java
@@ -1,5 +1,41 @@
+// package src;
+
+import car.Car;
+import factory.CarFactory;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static void main(String[] args) {
+
+        // Create Cars
+        Car gasCar = CarFactory.createCar("gas");
+        Car electricCar = CarFactory.createCar("electric");
+        Car hybridCar = CarFactory.createCar("hybrid");
+
+        System.out.println("\n--- GAS CAR TEST ---");
+        gasCar.start();
+        gasCar.accelerate();
+        gasCar.accelerate();
+        gasCar.brake();
+        gasCar.stop();
+
+        System.out.println("\n--- ELECTRIC CAR TEST ---");
+        electricCar.start();
+        electricCar.accelerate();
+        electricCar.stop();
+
+        System.out.println("\n--- HYBRID CAR TEST ---");
+        hybridCar.start();
+        hybridCar.accelerate();
+        hybridCar.accelerate();
+        hybridCar.accelerate();
+        hybridCar.brake();
+        hybridCar.stop();
+
+        System.out.println("\n--- ENGINE REPLACEMENT TEST ---");
+        Car car = CarFactory.createCar("gas");
+        car.start();
+        CarFactory.replaceEngine(car, "electric");
+        car.accelerate();
     }
 }
