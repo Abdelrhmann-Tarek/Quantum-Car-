0079dfcc5aa6c2f988ea0c3a7dbeaa9effec38f8
Abdelrhmann-Tarek <abdelrahmantarek696@gmail.com>
2026-04-24 16:28:11 +0300

    add ElectricEngine class to engines package

---
+++ src/engines/ElectricEngine.java
@@ -0,0 +1,28 @@
+package engines;
+
+public class ElectricEngine implements Engine {
+    private int speed = 0;
+
+    @Override
+    public void increase() {
+        speed += 1;
+        System.out.println("Electric Engine speed increased to: " + speed);
+    }
+
+    @Override
+    public void decrease() {
+        if (speed > 0) {
+            speed -= 1;
+            System.out.println("Electric Engine speed decreased to: " + speed);
+        } else {
+            System.out.println("Electric Engine is already at minimum speed.");
+        }
+    }
+
+    @Override
+    public void setSpeed(int speed) {
+        this.speed = speed;
+        System.out.println("Electric Engine speed set to: " + this.speed);
+    }
+
+}
