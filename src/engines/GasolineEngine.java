e05dbfa65530b4c702ead0d7d6d0a1508a7b0e67
Abdelrhmann-Tarek <abdelrahmantarek696@gmail.com>
2026-04-24 16:17:24 +0300

    edit GasolineEngine class to engines package

--- src/engines/GasolineEngine.java
+++ src/engines/GasolineEngine.java
@@ -1,5 +1,29 @@
 package engines;
 
-public class GasolineEngine {
+public class GasolineEngine implements Engine {
+    private int speed = 0;
+
+    @Override
+    public void increase() {
+        speed += 1;
+        System.out.println("Gasoline engine speed increased to " + speed);
+    }
+
+    @Override
+    public void decrease() {
+        if (speed > 0) {
+            speed -= 1;
+            System.out.println("Gasoline engine speed decreased to " + speed);
+        } else {
+            System.out.println("Gasoline engine is already at minimum speed.");
+
+        }
+    }
+
+    @Override
+    public void setSpeed(int speed) {
+        this.speed = speed;
+        System.out.println("Gasoline engine speed set to " + this.speed);
+    }
 
 }
